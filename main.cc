7867893c81ead3b3e979ab42ac644f5823b51bd2
K879 <1228342698@qq.com>
2023-10-30 17:36:47 +0100

    Use CMake to link a .exe with a static library.

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "test/sayHi.h"
 using namespace std;
 
 int main()
 {
-    cout << 10086;
+    sayHi();
+    cout << 10086 << endl;
     return 0;
 }
---
+++ test/sayHi.cc
@@ -0,0 +1,8 @@
+#include "sayHi.h"
+#include <iostream>
+
+int sayHi()
+{
+    std::cout << "Hi" << std::endl;
+    return 0;
+}
