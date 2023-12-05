1bb815c05cffc86d648f57cd2a8ed10921978558
K879 <1228342698@qq.com>
2023-12-05 12:45:22 +0100

    Add comments for GetOneGaussianByBoxMuller.

--- mylib/math/rng.cc
+++ mylib/math/rng.cc
@@ -15,16 +15,22 @@ double GetOneGaussianBySummation()
 
 double GetOneGaussianByBoxMuller()
 {
-    double result;
-    double x, y;
-    double sizeSquared;
+    double Z;
+    double u, v;
+    double U_1;
 
+    // Let U and V be i.i.d uniformly distributed r.v.s, following U(0,1)
+    //     U_1 = R^2 = U^2+V^2,
+    //     U_2 = Theta/(2*pi)
+    // Then, cos(2*pi*U_2) = cos(Theta) = u/R = u/sqrt(U_1)
+    // Therefore, Z = sqrt(-2*log(U_1))*cos(2*pi*U_2)
+    //              = u * sqrt(-2*log(U_1)/U_1)
     do {
-        x           = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
-        y           = 2.0 * rand() / static_cast<double>(RAND_MAX) - 1;
-        sizeSquared = x * x + y * y;
-    } while (sizeSquared >= 1.0);
-    result = x * sqrt(-2 * log(sizeSquared) / sizeSquared);
+        u   = 2.0 * rand() / ((double) RAND_MAX) - 1.0;
+        v   = 2.0 * rand() / ((double) RAND_MAX) - 1.0;
+        U_1 = u * u + v * v;
+    } while (U_1 >= 1.0);
 
-    return result;
+    Z = u * sqrt(-2 * log(U_1) / U_1);
+    return Z;
 }
